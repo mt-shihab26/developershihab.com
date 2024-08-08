812a8c35ea7752de592f3413f76b55337bd99713
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-08 16:00:11 +0600

    add content certifications and skills

--- src/site.config.ts
+++ src/site.config.ts
@@ -64,9 +64,48 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 	}
 };
 
+export const certifications = [
+	{
+		heading: 'Think in a Redux way Course (Learn with Sumit)',
+		subheading: `
+            Shihab Mahamud participated in Learn with Sumit Platform presents 
+            "Think in a Redux Way" course and performed exceptionally well. 
+            Not only that, he was one of the First Top 10 Students of the course. 
+            He completed all the modules very seriously and participated in all 
+            the quizzes & all the assignments with excellent results. He appeared in
+            the Final Examination of the course and scored 95.33% marks. Learn with 
+            Sumit Platform believes he can be an excellent resource for any web 
+            development company. We wish him continued success in life.
+        `,
+		date: 'Fab 2023 - Apr 2023',
+		imagePath: '/src/assets/learn-with-sumit.png',
+		altText: 'Think in a Redux way Course Certificate',
+		href: 'https://learnwithsumit.com/certificates/verify/LWSCTXN-RUP3O34K'
+	}
+] as const;
+
+export const skills = [
+	{
+		title: 'Languages',
+		tools: ['C/C++', 'JavaScript/TypeScript', 'PHP', 'Elixir']
+	},
+	{
+		title: 'Backend',
+		tools: ['Laravel', 'WordPress', 'Phoenix', 'MySQL', 'SQLite']
+	},
+	{
+		title: 'Frontend',
+		tools: ['React.js', 'Vue.js', 'TailwindCSS', 'shadcn/ui', 'Astro']
+	},
+	{
+		title: 'Others',
+		tools: ['Git', 'Neovim', 'Linux', 'Docker', 'Composer']
+	}
+] as const;
+
 export const socialLinks = {
 	linkedin: 'https://www.linkedin.com/in/shihab-mahamud',
 	x: 'https://x.com/shihabmahamud13',
 	github: 'https://github.com/p-nerd',
 	devto: 'https://dev.to/p-nerd'
-};
+} as const;
