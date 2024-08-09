6fd19dacfe86ffa667efc3a8c3da93b4f18e40ec
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-09 17:23:16 +0600

    blogs

--- src/utils/blogs.ts
+++ src/utils/blogs.ts
@@ -3,50 +3,42 @@ import { getCollection } from 'astro:content';
 
 export type TBlog = CollectionEntry<'post'>;
 
-/** Note: this function filters out draft posts based on the environment */
-export async function getAllPosts() {
+const getAllBlogs = async (): Promise<TBlog[]> => {
 	return await getCollection('post', ({ data }) => {
 		return import.meta.env.PROD ? data.draft !== true : true;
 	});
-}
+};
 
-export function sortMDByDate(posts: Array<CollectionEntry<'post'>>) {
-	return posts.sort((a, b) => {
+const sortMDByDate = (blogs: TBlog[]): TBlog[] => {
+	return blogs.sort((a, b) => {
 		const aDate = new Date(a.data.updatedDate ?? a.data.publishDate).valueOf();
 		const bDate = new Date(b.data.updatedDate ?? b.data.publishDate).valueOf();
 		return bDate - aDate;
 	});
-}
-
-/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
-export function getAllTags(posts: Array<CollectionEntry<'post'>>) {
-	return posts.flatMap((post) => [...post.data.tags]);
-}
-
-/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
-export function getUniqueTagsWithCount(
-	posts: Array<CollectionEntry<'post'>>
-): Array<[string, number]> {
-	return [
-		...getAllTags(posts).reduce(
-			(acc, t) => acc.set(t, (acc.get(t) || 0) + 1),
-			new Map<string, number>()
-		)
-	].sort((a, b) => b[1] - a[1]);
-}
-
-export const allBlogs = async () => {
-	return sortMDByDate(await getAllPosts());
 };
 
-/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
-export const getUniqueTags = (blogs: TBlog[]) => {
-	return [...new Set(getAllTags(blogs))];
+const getAllTags = async (): Promise<string[]> => {
+	return (await getAllBlogs()).flatMap((post) => [...post.data.tags]);
 };
 
-export const featuredPosts = async () => {
-	const posts = await getAllPosts();
-	const sorted = sortMDByDate(posts);
-	const sliced = sorted.slice(0, 10);
+export const allBlogs = async (): Promise<TBlog[]> => {
+	return sortMDByDate(await getAllBlogs());
+};
+
+export const featuredBlogs = async (): Promise<TBlog[]> => {
+	const posts = await allBlogs();
+	const sliced = posts.slice(0, 10);
 	return sliced;
+};
+
+export const getUniqueTags = async (): Promise<string[]> => {
+	const blogs = await getAllTags();
+	return [...new Set(blogs)];
+};
+
+export const getUniqueTagsWithCount = async (): Promise<[string, number][]> => {
+	const blogs = await getAllTags();
+	return [
+		...blogs.reduce((acc, t) => acc.set(t, (acc.get(t) || 0) + 1), new Map<string, number>())
+	].sort((a, b) => b[1] - a[1]);
 };
