23f0734909c7e8f7dd43b35f8f2dff2744c5f0fa
Shihab Mahamud <shihab4t@gmail.com>
2024-11-12 03:51:18 +0600

    projects

--- src/components/screens/projects/Projects.tsx
+++ src/components/screens/projects/Projects.tsx
@@ -14,17 +14,14 @@ const Projects = ({ projects, activeTab }: { projects: TProject[]; activeTab: TT
 			<Back href="/" />
 			<div className="flex flex-col">
 				<h1 className="mb-6 mt-5 text-2xl font-bold">My Projects</h1>
-				{projects.length === 0 ? (
-					<p>No projects yet.</p>
-				) : (
-					<TypeTabs value={activeTab}>
-						<div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-							{filteredProjects.map((project) => (
-								<Project key={project.id} project={project} />
-							))}
-						</div>
-					</TypeTabs>
-				)}
+				<TypeTabs value={activeTab}>
+					<div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+						{filteredProjects.length === 0 && <p>No projects yet.</p>}
+						{filteredProjects.map((project) => (
+							<Project key={project.id} project={project} />
+						))}
+					</div>
+				</TypeTabs>
 			</div>
 		</div>
 	);
