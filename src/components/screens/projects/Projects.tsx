4167e5d00e916ac52d79363a6c0c05fe5282a46c
Shihab Mahamud <shihab4t@gmail.com>
2024-11-12 03:37:14 +0600

    projects categories

--- src/components/screens/projects/Projects.tsx
+++ src/components/screens/projects/Projects.tsx
@@ -1,25 +1,32 @@
 import type { TProject } from '@/content/config';
 import type { TTypeTabsValue } from './TypeTabs';
 
-import { useState } from 'react';
-
+import Back from '@/components/Back';
 import Project from './Project';
 import TypeTabs from './TypeTabs';
 
-const Projects = ({ projects }: { projects: TProject[] }) => {
-	const [activeTab, setActiveTab] = useState<TTypeTabsValue>('all');
-
+const Projects = ({ projects, activeTab }: { projects: TProject[]; activeTab: TTypeTabsValue }) => {
 	const filteredProjects =
 		activeTab === 'all' ? projects : projects.filter((project) => project.data.type === activeTab);
 
 	return (
-		<TypeTabs value={activeTab} onChange={(value) => setActiveTab(value)}>
-			<div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-				{filteredProjects.map((project) => (
-					<Project key={project.id} project={project} />
-				))}
+		<div className="w-full">
+			<Back href="/" />
+			<div className="flex flex-col">
+				<h1 className="mb-6 mt-5 text-2xl font-bold">My Projects</h1>
+				{projects.length === 0 ? (
+					<p>No projects yet.</p>
+				) : (
+					<TypeTabs value={activeTab}>
+						<div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+							{filteredProjects.map((project) => (
+								<Project key={project.id} project={project} />
+							))}
+						</div>
+					</TypeTabs>
+				)}
 			</div>
-		</TypeTabs>
+		</div>
 	);
 };
 
--- src/components/screens/projects/TypeTabs.tsx
+++ src/components/screens/projects/TypeTabs.tsx
@@ -5,28 +5,28 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export type TTypeTabsValue = TType | 'all';
 
-const TypeTabs = ({
-	value,
-	onChange,
-	children
-}: {
-	value: TTypeTabsValue;
-	onChange: (value: TTypeTabsValue) => void;
-	children: TNode;
-}) => {
+const TypeTabs = ({ value, children }: { value: TTypeTabsValue; children: TNode }) => {
 	return (
-		<Tabs value={value} onValueChange={(value: TTypeTabsValue) => onChange(value)}>
+		<Tabs value={value}>
 			<TabsList className="mb-8">
-				<TabsTrigger value="all">
-					All <span className="ml-1 hidden lg:block">Projects</span>
-				</TabsTrigger>
-				<TabsTrigger value="client">
-					Client <span className="ml-1 hidden lg:block">Projects</span>
-				</TabsTrigger>
-				<TabsTrigger value="demo">
-					Demo <span className="ml-1 hidden lg:block">Projects</span>
-				</TabsTrigger>
-				<TabsTrigger value="tool">Tools</TabsTrigger>
+				<a href="/projects">
+					<TabsTrigger value="all">
+						All <span className="ml-1 hidden lg:block">Projects</span>
+					</TabsTrigger>
+				</a>
+				<a href="/projects/clients">
+					<TabsTrigger value="client">
+						Client <span className="ml-1 hidden lg:block">Projects</span>
+					</TabsTrigger>
+				</a>
+				<a href="/projects/demos">
+					<TabsTrigger value="demo">
+						Demo <span className="ml-1 hidden lg:block">Projects</span>
+					</TabsTrigger>
+				</a>
+				<a href="/projects/tools">
+					<TabsTrigger value="tool">Tools</TabsTrigger>
+				</a>
 			</TabsList>
 			<TabsContent value={value}>{children}</TabsContent>
 		</Tabs>
