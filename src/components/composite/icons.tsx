852b3ea2bcd9e1d8ad6df98ac05031a5d5cdf3eb
Shihab Mahamud <shihab4t@gmail.com>
2024-11-21 02:58:08 +0600

    fix images

--- src/components/composite/icons.tsx
+++ src/components/composite/icons.tsx
@@ -4,6 +4,7 @@ export { ExternalLinkIcon, GithubIcon, InfoIcon };
 
 import ChatgptIcon from '@/components/icons/ChatgptIcon';
 import ClaudeIcon from '@/components/icons/ClaudeIcon';
+import CompanyIcon from '@/components/icons/CompanyIcon';
 import CppIcon from '@/components/icons/CppIcon';
 import DockerIcon from '@/components/icons/DockerIcon';
 import ElixirIcon from '@/components/icons/ElixirIcon';
@@ -11,7 +12,9 @@ import GitIcon from '@/components/icons/GitIcon';
 import GoIcon from '@/components/icons/GoIcon';
 import KittyIcon from '@/components/icons/KittyIcon';
 import LaravelIcon from '@/components/icons/LaravelIcon';
+import LinkedinIcon from '@/components/icons/LinkedinIcon';
 import LinuxIcon from '@/components/icons/LinuxIcon';
+import LocationIcon from '@/components/icons/LocationIcon';
 import LuaIcon from '@/components/icons/LuaIcon';
 import NeovimIcon from '@/components/icons/NeovimIcon';
 import PhoenixIcon from '@/components/icons/PhoenixIcon';
@@ -27,6 +30,7 @@ import WordpressIcon from '@/components/icons/WordpressIcon';
 export {
 	ChatgptIcon,
 	ClaudeIcon,
+	CompanyIcon,
 	CppIcon,
 	DockerIcon,
 	ElixirIcon,
@@ -34,7 +38,9 @@ export {
 	GoIcon,
 	KittyIcon,
 	LaravelIcon,
+	LinkedinIcon,
 	LinuxIcon,
+	LocationIcon,
 	LuaIcon,
 	NeovimIcon,
 	PhoenixIcon,
