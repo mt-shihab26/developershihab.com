05d63970c6eff64e3291d62748edb1c3b756af5c
Shihab Mahamud <shihab4t@gmail.com>
2024-11-21 01:13:43 +0600

    convert to shadcn

--- src/components/screens/resume/Education.tsx
+++ src/components/screens/resume/Education.tsx
@@ -1,5 +1,7 @@
 import type { TEducation } from '@/types';
 
+import { Card, CardContent } from '@/components/ui/card';
+
 import educations from '@/config/educations';
 
 import Section from './Section';
@@ -8,39 +10,39 @@ const Item = ({ education }: { education: TEducation }) => {
 	const { institute, department, date, logo, alt, description, points } = education;
 
 	return (
-		<div className="relative rounded-2xl border border-border bg-primary-foreground px-5 py-3 text-muted-foreground">
-			{logo && (
-				<img
-					src={logo}
-					alt={alt || ''}
-					className="mb-3 h-12 w-auto lg:absolute lg:-left-16 lg:mb-0"
-				/>
-			)}
-			<div className="flex flex-col gap-y-1.5">
-				<h2 className="text-lg font-medium text-foreground">{institute}</h2>
-				<p>{department}</p>
-				<p>{description}</p>
-				{points && points.length > 0 && (
-					<ul className="ml-4 list-disc">
-						{points.map((point, index) => (
-							<li key={index}>{point}</li>
-						))}
-					</ul>
+		<Card className="relative bg-primary-foreground">
+			<CardContent className="p-5">
+				{logo && (
+					<img
+						src={logo}
+						alt={alt || ''}
+						className="mb-3 h-12 w-auto lg:absolute lg:-left-16 lg:mb-0"
+					/>
 				)}
-				<time>{date}</time>
-			</div>
-		</div>
+				<div className="flex flex-col gap-y-1.5">
+					<h2 className="text-lg font-medium text-foreground">{institute}</h2>
+					<p className="text-muted-foreground">{department}</p>
+					<p className="text-muted-foreground">{description}</p>
+					{points && points?.length > 0 && (
+						<ul className="ml-4 list-disc text-muted-foreground">
+							{points.map((point, index) => (
+								<li key={index}>{point}</li>
+							))}
+						</ul>
+					)}
+					<time className="text-muted-foreground">{date}</time>
+				</div>
+			</CardContent>
+		</Card>
 	);
 };
 
-const Education = () => {
-	return (
-		<Section title="Education">
-			{educations.map((education) => (
-				<Item education={education} />
-			))}
-		</Section>
-	);
-};
+const Education = () => (
+	<Section title="Education">
+		{educations.map((education, index) => (
+			<Item key={index} education={education} />
+		))}
+	</Section>
+);
 
 export default Education;
