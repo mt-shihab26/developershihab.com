32f1e9aed96632dd8a823b174cfc3e77b8f60010
Shihab Mahamud <shihab4t@gmail.com>
2024-11-23 00:59:21 +0600

    hero section final

--- src/components/screens/resume/Hero.tsx
+++ src/components/screens/resume/Hero.tsx
@@ -12,34 +12,34 @@ import Label from "./Label";
 
 const Hero = () => {
     return (
-        <section className="flex flex-col items-center gap-y-7">
-            <img
-                src={portrait.src}
-                alt="Shihab Mahamud Profile"
-                className="h-28 w-auto rounded-full bg-[#FFBE98] p-2"
-                loading="eager"
-            />
-            <div className="flex flex-col items-center gap-y-4">
-                <p className="text-[10px] text-muted-foreground lg:text-base">
+        <section className="flex flex-col items-center space-y-5">
+            <div className="flex flex-col items-center space-y-2">
+                <img
+                    src={portrait.src}
+                    alt="Shihab Mahamud Profile"
+                    className="h-28 w-auto rounded-full bg-[#FFBE98] p-2"
+                    loading="eager"
+                />
+                <h2 className="text-[10px] text-muted-foreground lg:text-base">
                     Programmer | Problem Solver | Software Engineer
-                </p>
-                <p className="mb-5 px-2 text-center text-xl lg:w-3/4 lg:px-0 lg:text-3xl">
-                    <B>Hello, I'm Shihab.</B> I'm a <B>full-stack developer</B> with <B>3 years</B>{" "}
-                    of experience, and I enjoy building for <I>the web</I>. My focus is{" "}
-                    <I>Laravel</I> & <I>React</I>
-                </p>
-                <ContactEmail />
-                <div className="flex flex-wrap justify-center gap-x-7 gap-y-3">
-                    <Label title="Finnext.co" href="https://finnext.co" target="_blank">
-                        <CompanyIcon className="h-5 w-5" />
-                    </Label>
-                    <Label title="Dhaka, Bangladesh">
-                        <LocationIcon className="h-5 w-5" />
-                    </Label>
-                    <Label title="Connect on Linkedin" href={socials.linkedin} target="_blank">
-                        <LinkedinIcon className="h-5 w-5 text-foreground/75" />
-                    </Label>
-                </div>
+                </h2>
+            </div>
+            <p className="mb-5 px-2 text-center text-xl lg:px-16 lg:text-3xl">
+                <B>Hello, I'm Shihab.</B> I'm a <B>full-stack developer</B> with <B>3 years</B> of
+                experience, and I enjoy building for <I>the web</I>. My stacks is <I>Laravel</I> &{" "}
+                <I>React</I>
+            </p>
+            <ContactEmail />
+            <div className="flex flex-wrap justify-center gap-x-7 gap-y-3">
+                <Label title="Finnext" href="https://finnext.co" target="_blank">
+                    <CompanyIcon className="h-5 w-5" />
+                </Label>
+                <Label title="Dhaka, Bangladesh">
+                    <LocationIcon className="h-5 w-5" />
+                </Label>
+                <Label title="Connect on Linkedin" href={socials.linkedin} target="_blank">
+                    <LinkedinIcon className="h-5 w-5 text-foreground/75" />
+                </Label>
             </div>
         </section>
     );
