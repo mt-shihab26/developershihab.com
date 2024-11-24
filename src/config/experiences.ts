2bec42c438dbc3b3b91911e392d272588a577a91
Shihab Mahamud <shihab4t@gmail.com>
2024-11-25 01:26:31 +0600

    fixing experiences content

--- src/config/experiences.ts
+++ src/config/experiences.ts
@@ -19,47 +19,27 @@ export type TExperience = {
 const experiences: TExperience[] = [
     {
         firm: {
-            name: "Finnext.co",
+            name: "Finnext",
             link: "https://finnext.co",
             logo: finnext,
-            alt: "Finnext.co logo"
+            alt: "Finnext logo"
         },
         roles: [
             {
                 position: "CTO",
-                date: "May 2023 - Present",
+                date: "Sep 2024 - Present",
                 description: `
-                    I worked at Finnext.co, where I provided custom software solutions.
-                    Although primarily a web design agency focused on custom design and
-                    branding, they occasionally encountered clients needing custom
-                    software solutions. I joined as a Full-stack Web Application Developer,
-                    responsible for developing and maintaining these solutions. My main
-                    project was PaystubHero.com, a SaaS payroll document generator for
-                    American employees. I initially built this platform using React and
-                    a WordPress plugin. After 1.5 years of growth, I rewrote the entire
-                    platform using the Laravel framework with React. I still continued
-                    to maintain this application, fixing bugs and adding new features.
-                    I also contributed to other projects. Below is a list of notable
-                    projects I developed or maintained here.
+                    Responsible for defining technical strategy, managing the development
+                    team, and delivering scalable software solutions.
                 `,
-                projects: ["paystubhero-wordpress-plugin"]
+                projects: []
             },
             {
                 position: "Full-stack Developer",
-                date: "May 2023 - Present",
+                date: "May 2023 - Sep 2024 | 1 yr 5 mos",
                 description: `
-                    I worked at Finnext.co, where I provided custom software solutions.
-                    Although primarily a web design agency focused on custom design and
-                    branding, they occasionally encountered clients needing custom
-                    software solutions. I joined as a Full-stack Web Application Developer,
-                    responsible for developing and maintaining these solutions. My main
-                    project was PaystubHero.com, a SaaS payroll document generator for
-                    American employees. I initially built this platform using React and
-                    a WordPress plugin. After 1.5 years of growth, I rewrote the entire
-                    platform using the Laravel framework with React. I still continued
-                    to maintain this application, fixing bugs and adding new features.
-                    I also contributed to other projects. Below is a list of notable
-                    projects I developed or maintained here.
+                    Designed and developed full-stack applications, ensuring
+                    high performance and maintainability across platforms.
                 `,
                 projects: ["paystubhero-wordpress-plugin"]
             }
@@ -77,13 +57,8 @@ const experiences: TExperience[] = [
                 position: "Software Engineer - Intern",
                 date: "June 2024 - September 2024",
                 description: `
-                    I joined Systech Digital LTD as an Intern Software Engineer
-                    during my Diploma final semester's industrial training program. Initially,
-                    I developed demo projects, and later, I contributed to client
-                    projects. My work mainly involved the Laravel framework. This
-                    internship significantly enhanced my Laravel development skills
-                    and expanded my understanding of modern web technologies.
-                    Below is a list of projects I did here.
+                    Contributed to demo and client projects using Laravel, enhancing
+                    my technical skills and understanding of web development.
                 `,
                 projects: ["s-commerce", "student-performance-monitoring-system"]
             }
