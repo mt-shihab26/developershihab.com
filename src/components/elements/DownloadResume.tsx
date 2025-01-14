11cb1203e32a55e48e32397815014f9d16df0d3f
Shihab Mahamud <shihab4t@gmail.com>
2025-01-15 05:17:45 +0600

    pdf generate

--- src/components/elements/DownloadResume.tsx
+++ src/components/elements/DownloadResume.tsx
@@ -1,12 +1,10 @@
 import DownloadIcon from "@/components/icons/DownloadIcon";
-import ResumeModal from "./ResumeModal";
 
 const DownloadResume = () => {
     return (
         <div className="inline-flex flex-row items-center gap-x-3 rounded-3xl border border-input px-4 py-2 text-sm shadow-sm transition-all hover:shadow-md">
             <DownloadIcon className="h-4 w-4" />
             <span className="font-medium">Download Resume</span>
-            <ResumeModal />
         </div>
     );
 };
