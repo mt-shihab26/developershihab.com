6173c616ce2c9d48e01afb6915185eb29e502f87
Shihab Mahamud <shihab4t@gmail.com>
2025-02-21 05:34:26 +0600

    avatar header

--- src/components/layouts/site-layout/header/avatar.tsx
+++ src/components/layouts/site-layout/header/avatar.tsx
@@ -1,39 +1,43 @@
+import type { TAProps, TDivProps } from "~/types/utils";
+
+import { splitProps } from "solid-js";
 import { cn } from "~/lib/utils";
 
-import shihabImage from "@/images/shihab.jpg";
+import shihabImage from "~/assets/images/shihab.jpg";
 
-export function AvatarContainer(props: any) {
+const AvatarContainer = (props: { class?: string } & TDivProps) => {
+    const [local, others] = splitProps(props, ["class"]);
+
     return (
         <div
             class={cn(
                 "h-10 w-10 rounded-full bg-white/90 p-0.5 ring-1 shadow-lg shadow-zinc-800/5",
                 "ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10",
-                props.class,
+                local.class
             )}
-            {...props}
+            {...others}
         />
     );
-}
+};
 
-export function Avatar({ large = false, class, ...props }) {
+const Avatar = (props: { large?: boolean; class?: string } & TAProps) => {
+    const [local, others] = splitProps(props, ["large", "class"]);
+
+    const large = () => local.large ?? false;
+
     return (
-        <a
-            href="/"
-            aria-label="Home"
-            class={cn(className, "pointer-events-auto")}
-            {...props}
-        >
+        <a href="/" aria-label="Home" class={cn(local.class, "pointer-events-auto")} {...others}>
             <img
                 src={shihabImage.src}
                 alt=""
-                sizes={large ? "4rem" : "2.25rem"}
+                sizes={large() ? "4rem" : "2.25rem"}
                 class={cn(
                     "rounded-full bg-zinc-100 object-cover dark:bg-zinc-800",
-                    large ? "h-16 w-16" : "h-9 w-9"
+                    large() ? "h-16 w-16" : "h-9 w-9"
                 )}
             />
         </a>
     );
-}
+};
 
-
+export { Avatar, AvatarContainer };
--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -3,7 +3,8 @@ import type { JSX } from "solid-js";
 type TAsProps = keyof JSX.IntrinsicElements | Component;
 type TAProps = JSX.AnchorHTMLAttributes<HTMLAnchorElement>;
 type TSvgProps = JSX.SvgSVGAttributes<SVGSVGElement>;
+type TDivProps = JSX.HTMLAttributes<HTMLDivElement>;
 
 type TIcon = (props: TSvgProps) => JSX.Element;
 
-export { TAProps, TAsProps, TIcon };
+export { TAProps, TAsProps, TDivProps, TIcon };
