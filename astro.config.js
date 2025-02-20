dd25191a232bfef64c4a8375ebb5585845bc09c3
Shihab Mahamud <shihab4t@gmail.com>
2025-02-21 00:31:31 +0600

    add tailwind intregration

--- astro.config.js
+++ astro.config.js
@@ -4,8 +4,11 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [solid()],
     adapter: vercel({ webAnalytics: { enabled: true } }),
+    vite: { plugins: [tailwindcss()] },
 });
