3869bbc4058dc4167b5b41e84459b898b4ff9a5b
Shihab Mahamud <shihab4t@gmail.com>
2025-02-21 00:24:55 +0600

    add vercel adapter

--- astro.config.js
+++ astro.config.js
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import solidJs from "@astrojs/solid-js";
+import solid from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [solidJs()],
+    integrations: [solid()],
+    adapter: vercel({ webAnalytics: { enabled: true } }),
 });
