41329d83e52f0856a94be7578834f6f0063c3c30
Shihab Mahamud <shihab4t@gmail.com>
2025-02-21 14:40:33 +0600

    mobile navigation

--- src/components/screens/layout/header/mobile-navigation.tsx
+++ src/components/screens/layout/header/mobile-navigation.tsx
@@ -1,6 +1,5 @@
-import type { JSXElement } from "solid-js";
-
 import { createSignal } from "solid-js";
+import { cn } from "~/lib/utils";
 import { links } from "./links";
 
 import { For, Show } from "solid-js";
@@ -8,21 +7,11 @@ import { Transition } from "terracotta";
 import { ChevronDownIcon } from "~/components/icons/chevron-down-icon";
 import { CloseIcon } from "~/components/icons/close-icon";
 
-const Item = (props: { href: string; children: JSXElement }) => {
-    return (
-        <li>
-            <a href={props.href} class="block py-2">
-                {props.children}
-            </a>
-        </li>
-    );
-};
-
 const MobileNavigation = (props: { pathname: string; class?: string }) => {
     const [open, setOpen] = createSignal<boolean>(false);
 
     return (
-        <div class={`relative ${props.class || ""}`}>
+        <div class={cn("relative", props.class)}>
             <button
                 onClick={() => setOpen(!open())}
                 class="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20"
@@ -30,7 +19,6 @@ const MobileNavigation = (props: { pathname: string; class?: string }) => {
                 Menu
                 <ChevronDownIcon class="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
             </button>
-
             <Show when={open()}>
                 <Transition
                     show={open()}
@@ -46,7 +34,6 @@ const MobileNavigation = (props: { pathname: string; class?: string }) => {
                         onClick={() => setOpen(false)}
                     />
                 </Transition>
-
                 <div
                     class="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800"
                     onClick={(e) => e.stopPropagation()}
@@ -66,7 +53,13 @@ const MobileNavigation = (props: { pathname: string; class?: string }) => {
                     <nav class="mt-6">
                         <ul class="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                             <For each={links}>
-                                {(link) => <Item href={link.href}>{link.label}</Item>}
+                                {(link) => (
+                                    <li>
+                                        <a href={link.href} class="block py-2">
+                                            {link.label}
+                                        </a>
+                                    </li>
+                                )}
                             </For>
                         </ul>
                     </nav>
