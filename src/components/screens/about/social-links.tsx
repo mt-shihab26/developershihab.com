922e6e02c6966a60cd056bfb0af2990aef9a12c5
Shihab Mahamud <shihab4t@gmail.com>
2025-02-25 02:25:17 +0600

    about page

--- src/components/screens/about/social-links.tsx
+++ src/components/screens/about/social-links.tsx
@@ -15,6 +15,7 @@ const SocialLink = (props: { class?: string; href: string; children: JSXElement;
             <a
                 href={props.href}
                 class="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+                target="_blank"
             >
                 <Icon class="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
                 <span class="ml-4">{props.children}</span>
