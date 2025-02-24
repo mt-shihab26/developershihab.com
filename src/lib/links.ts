350f32a63818c27a60d24794c13f282405e44a35
Shihab Mahamud <shihab4t@gmail.com>
2025-02-25 05:40:59 +0600

    description content updated

---
+++ src/lib/links.ts
@@ -0,0 +1,18 @@
+const links = {
+    cpbook: "http://cpbook.subeen.com",
+    online_judges: "https://github.com/p-nerd/online_judges",
+    dsa_library: "https://github.com/p-nerd/dsa_library",
+    laravel: "https://laravel.com",
+    wordpress: "https://wordpress.org",
+    react: "https://react.dev",
+    finnext: "https://finnext.co",
+    typescript: "https://www.typescriptlang.org",
+    php: "https://php.net",
+    tailwindcss: "https://tailwindcss.com",
+    git: "https://git-scm.com",
+    ubuntu: "https://ubuntu.com",
+    neovim: "https://neovim.io",
+    tmux: "https://github.com/tmux/tmux"
+};
+
+export { links };
