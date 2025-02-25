fef9c4091ebf7f6d57138ac6960b3f12f476cf05
Shihab Mahamud <shihab4t@gmail.com>
2025-02-25 08:04:14 +0600

    add prefetching

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -22,7 +22,7 @@ const CardLink = (props: { children: JSX.Element; href: string; target?: "_blank
     return (
         <>
             <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50" />
-            <a href={props.href} target={props.target}>
+            <a href={props.href} target={props.target} data-astro-prefetch>
                 <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
                 <span class="relative z-10">{props.children}</span>
             </a>
