fa881ba884b917988a83ab6a6097fa85bec1a3c0
Shihab Mahamud <shihab4t@gmail.com>
2025-03-04 01:41:50 +0600

    convert button component to react

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from "solid-js";
+import type { ReactNode } from "react";
 
 import { cn } from "~/lib/utils";
 
@@ -11,23 +11,31 @@ const variantStyles: Record<TVariant, string> = {
         "bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70"
 };
 
-const Button = (props: {
+const Button = ({
+    type,
+    children,
+    variant,
+    className,
+    href
+}: {
     type?: "submit" | "button";
-    children?: JSX.Element;
+    children?: ReactNode;
     variant?: TVariant;
-    class?: string;
+    className?: string;
     href?: string;
 }) => {
     const clases = cn(
         "inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none",
-        variantStyles[props.variant || "primary"],
-        props.class
+        variantStyles[variant || "primary"],
+        className
     );
 
-    return props.href ? (
-        <a href={props.href} class={clases} {...props} />
+    return href ? (
+        <a href={href} className={clases}>
+            {children}
+        </a>
     ) : (
-        <button class={clases} {...props} />
+        <button className={clases} type={type} />
     );
 };
 
