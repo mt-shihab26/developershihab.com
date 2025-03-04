ce31066cbbaa93bb47deac90946dde059745079a
Shihab Mahamud <shihab4t@gmail.com>
2025-03-04 18:31:48 +0600

    resume

--- src/components/screens/resume/index.tsx
+++ src/components/screens/resume/index.tsx
@@ -9,7 +9,7 @@ const base = (resume: string) => {
             <head>
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Shihab Mahamud Resume</title>
+                <title>shihab-mahamud-resume.pdf</title>
                 <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
             </head>
             <body>${resume}</body>
