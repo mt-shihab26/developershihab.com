37c4c3fa6cf79777f96a56b0ed5628057f727a64
Shihab Mahamud <shihab4t@gmail.com>
2025-03-04 16:27:26 +0600

    header fixed

--- src/components/screens/resume/header.tsx
+++ src/components/screens/resume/header.tsx
@@ -2,15 +2,15 @@ import { meta } from "~/lib/meta";
 
 const Header = () => {
     return (
-        <header className="flex text-sm leading-1 items-center justify-between">
+        <header className="text-sm leading-5 flex items-center justify-between">
             <div className="space-y-1">
                 <h1 className="text-4xl font-bold">{meta.name}</h1>
                 <p>Full-stack developer having 3+ years of experience</p>
             </div>
-            <div>
+            <div className="font-medium">
                 <div>Dhaka, Bangladesh</div>
-                <div className="font-medium">{meta.email}</div>
-                <div className="font-medium">{meta.phone}</div>
+                <div>{meta.email}</div>
+                <div>{meta.phone}</div>
             </div>
         </header>
     );
