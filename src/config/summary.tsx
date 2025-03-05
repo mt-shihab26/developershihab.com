89c862a9d4dffd34b2f117658c386b3bebc7a6d5
Shihab Mahamud <shihab4t@gmail.com>
2025-03-05 20:11:17 +0600

    fix summary

--- src/config/summary.tsx
+++ src/config/summary.tsx
@@ -3,9 +3,10 @@ const SummaryParagraph = () => (
         Full-stack developer with expertise in Laravel, React, and WordPress, complemented by a
         strong foundation in data structures and algorithms from competitive programming experience
         (1100+ problems). Specialized in building dynamic applications using JavaScript/TypeScript,
-        PHP, and modern development workflows. Currently working with Finnext on paystubhero.com, a
-        SaaS Application. Committed to clean, maintainable code and best practices while seeking
-        opportunities to contribute to meaningful projects that simplify lives through technology.
+        PHP, and modern development workflows. Currently working with Finnext on various projects
+        (include paystubhero.com, quickdoctorsnote.com, etc). Committed to clean, maintainable code
+        and best practices while seeking opportunities to contribute to meaningful projects that
+        simplify lives through technology.
     </>
 );
 
