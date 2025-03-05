98cb72f73ec50cb2891800225c72348b960554b0
Shihab Mahamud <shihab4t@gmail.com>
2025-03-05 15:47:04 +0600

    make buttons white

--- src/components/screens/resume/back-button.tsx
+++ src/components/screens/resume/back-button.tsx
@@ -6,10 +6,10 @@ export const BackButton = () => {
             <button
                 type="button"
                 aria-label="Go back to articles"
-                className="group cursor-pointer flex h-10 w-10 items-center justify-center rounded-full shadow-md shadow-zinc-800/5 transition border border-zinc-700/50 bg-zinc-800 ring-0 ring-white/10 hover:border-zinc-700 hover:ring-white/20"
+                className="cursor-pointer flex h-10 w-10 items-center justify-center rounded-full shadow-xs transition border border-zinc-700/50 bg-white text-black "
                 onClick={() => history.back()}
             >
-                <ArrowLeftIcon className="h-4 w-4 transition stroke-zinc-200 group-hover:stroke-zinc-300" />
+                <ArrowLeftIcon className="h-4 w-4 transition stroke-black" />
             </button>
         </div>
     );
