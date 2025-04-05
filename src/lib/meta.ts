67ce4b6e1ed4abe7924bcd8ab5bbd95e32184637
Shihab Mahamud <shihab4t@gmail.com>
2025-04-06 03:21:08 +0600

    update x url

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -7,7 +7,7 @@ const meta = {
     phone: "+880 17 58-776344",
     github: "https://github.com/p-nerd",
     linkedin: "https://linkedin.com/in/shihab-mahamud",
-    x: "https://x.com/shihab4t",
+    x: "https://x.com/_shihab_2002",
     instagram: "https://instagram.com/shihab.mahamud.2002",
     facebook: "https://web.facebook.com/shihab.mahamud.2002"
 };
