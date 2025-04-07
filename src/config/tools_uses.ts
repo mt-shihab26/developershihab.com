661cd391f68e88cea5275c1523252af268d7b984
Shihab Mahamud <shihab4t@gmail.com>
2025-04-08 02:05:05 +0600

    update

--- src/config/tools_uses.ts
+++ src/config/tools_uses.ts
@@ -133,7 +133,7 @@ const tools_uses = [
         title: "Devices",
         items: [
             {
-                name: "13-inch, MacBook Pro, M2, 8GB RAM (2022 )",
+                name: "13-inch, MacBook Pro, M2, 8GB RAM (2022)",
                 description:
                     "Coming from an Avita laptop running Ubuntu-based Linux, the M2 MacBook provides better performance, seamless macOS integration, excellent battery life, and superior build quality."
             }
