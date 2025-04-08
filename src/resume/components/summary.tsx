2c71badb07911b3309bd92bbc1163407179c4322
Shihab Mahamud <shihab4t@gmail.com>
2025-04-08 21:34:03 +0600

    wip

--- src/resume/components/summary.tsx
+++ src/resume/components/summary.tsx
@@ -4,9 +4,9 @@ import { Heading } from "./heading";
 
 const Summary = () => {
     return (
-        <section className="space-y-3">
+        <section className="text-sm space-y-3">
             <Heading>SUMMARY</Heading>
-            <div className="text-xs space-y-2">
+            <div className="space-y-2">
                 <SummaryParagraph />
             </div>
         </section>
