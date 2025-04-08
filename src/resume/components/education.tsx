0aed30fb9b0faed731e8f2d6361e390945d26836
Shihab Mahamud <shihab4t@gmail.com>
2025-04-08 22:42:11 +0600

    wip

--- src/resume/components/education.tsx
+++ src/resume/components/education.tsx
@@ -4,13 +4,13 @@ import { Heading } from "./heading";
 
 const Education = () => {
     return (
-        <section className="space-y-3">
+        <section className="space-y-3 text-sm">
             <Heading>Education</Heading>
             {educations.map((edu, index) => (
-                <div key={index} className="text-xs">
-                    <div className="font-semibold">{edu.institution}</div>
-                    <div>{edu.location}</div>
-                    <div>{edu.degree}</div>
+                <div key={index} className="flex text-xs items-center space-x-2">
+                    <div className="font-semibold text-sm">{edu.institution} | </div>
+                    <div>{edu.location} | </div>
+                    <div>{edu.degree} | </div>
                     <div>{edu.period}</div>
                 </div>
             ))}
