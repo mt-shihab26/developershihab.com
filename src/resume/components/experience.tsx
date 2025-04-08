b8c276dc8c06f3a33a834a154eccfdfde10db9a0
Shihab Mahamud <shihab4t@gmail.com>
2025-04-08 21:24:32 +0600

    wip

--- src/resume/components/experience.tsx
+++ src/resume/components/experience.tsx
@@ -5,7 +5,7 @@ import { Heading } from "./heading";
 const Experience = () => {
     return (
         <section className="space-y-3">
-            <Heading>Work Experience</Heading>
+            <Heading>Experience</Heading>
             {experiences.map((e, index) => (
                 <div key={index} className="space-y-2">
                     <div>
