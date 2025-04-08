7931b93c3e2298cb933c62f69ca4e593f26fc265
Shihab Mahamud <shihab4t@gmail.com>
2025-04-08 21:22:48 +0600

    seperator

--- src/resume/components/header.tsx
+++ src/resume/components/header.tsx
@@ -1,40 +1,42 @@
 import { meta } from "~/lib/meta";
 
+const Seperator = () => {
+    return <span className="mx-2">•</span>;
+};
+
 const Header = ({ image }: { image?: boolean }) => {
     return (
-        <header className="text-sm">
+        <header className="space-y-1 w-full items-center text-sm">
             {image && (
                 <div className="flex flex-col items-center justify-center">
                     <img
                         src="https://avatars.githubusercontent.com/u/67628903?v=4"
-                        className="h-22 mr-2"
+                        className="h-40"
                     />
                 </div>
             )}
-            <div className="space-y-1 w-full items-center">
-                <h1 className="text-4xl text-center font-bold">{meta.name}</h1>
-                <div className="text-center">
-                    <span>Dhaka, Bangladesh</span>
-                    <span> </span>
-                    <span>{meta.phone}</span>
-                    <span> </span>
-                    <a href={`mailto:${meta.email}`} target="_blank">
-                        {meta.email}
-                    </a>
-                </div>
-                <div className="text-center">
-                    <a href={meta.url} target="_blank">
-                        developershihab.com
-                    </a>
-                    <span> </span>
-                    <a href={meta.github} target="_blank">
-                        github
-                    </a>
-                    <span> </span>
-                    <a href={meta.linkedin} target="_blank">
-                        linkedin
-                    </a>
-                </div>
+            <h1 className="text-4xl text-center font-bold">{meta.name}</h1>
+            <div className="text-center">
+                <span>Dhaka, Bangladesh</span>
+                <Seperator />
+                <span>{meta.phone}</span>
+                <Seperator />
+                <a href={`mailto:${meta.email}`} target="_blank">
+                    {meta.email}
+                </a>
+            </div>
+            <div className="text-center">
+                <a href={meta.url} target="_blank">
+                    developershihab.com
+                </a>
+                <Seperator />
+                <a href={meta.github} target="_blank">
+                    github
+                </a>
+                <Seperator />
+                <a href={meta.linkedin} target="_blank">
+                    linkedin
+                </a>
             </div>
         </header>
     );
