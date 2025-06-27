2e638f6bf7d82140fd155c105ddf05f7e8d472a9
Shihab Mahamud <shihab4t@gmail.com>
2025-06-28 01:34:53 +0600

    update meta info

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -5,11 +5,12 @@ const meta = {
         "I'm Shihab. I am a software developer from bangladesh. I mainly work with Laravel, React & WordPress.",
     email: "shihab4t@gmail.com",
     phone: "+880 1758-776344",
-    github: "https://github.com/p-nerd",
-    linkedin: "https://linkedin.com/in/shihab-mahamud",
-    x: "https://x.com/_shihab_2002",
-    instagram: "https://instagram.com/shihab.mahamud.2002",
-    facebook: "https://web.facebook.com/shihab.mahamud.2002"
+    github: "https://github.com/mt-shihab26",
+    linkedin: "https://linkedin.com/in/mt-shihab26",
+    x: "https://x.com/mt_shihab26",
+    instagram: "https://instagram.com/mt.shihab26",
+    facebook: "https://web.facebook.com/mt.shihab26",
+    dev_to: "https://dev.to/mt-shihab26"
 };
 
 export { meta };
