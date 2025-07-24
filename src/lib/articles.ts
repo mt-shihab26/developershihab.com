673d0981dd03072de8e244ff3f3e7e4d065e57e6
Shihab Mahamud <shihab4t@gmail.com>
2025-07-25 04:13:34 +0600

    articles page

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -2,20 +2,14 @@ import type { CollectionEntry } from "astro:content";
 
 import { getCollection } from "astro:content";
 
-export type TArticle = CollectionEntry<"articles">;
-
 const filterPublished = (articles: TArticle[]) => {
-    if (import.meta.env.DEV) {
-        return articles;
-    }
+    if (import.meta.env.DEV) return articles;
     return articles.filter((article) => article.data.published);
 };
+
+export type TArticle = CollectionEntry<"articles">;
 
 export const getArticles = async (): Promise<TArticle[]> => {
     const articles: TArticle[] = await getCollection("articles");
     return filterPublished(articles).sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
-};
-
-export const getFeaturedArticles = async (): Promise<TArticle[]> => {
-    return (await getArticles()).filter((article) => article.data.featured);
 };
