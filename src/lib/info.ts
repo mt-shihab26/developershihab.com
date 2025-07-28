d11a9d7ae113376d25cd4782a27672b6fdc1f89c
Shihab Mahamud <shihab4t@gmail.com>
2025-07-29 05:08:42 +0600

    fixing

--- src/lib/info.ts
+++ src/lib/info.ts
@@ -1,3 +1,3 @@
 export const name = "Shihab Mahamud";
-export const bio = "Programmer, Full Stack Developer, Software Engineer";
+export const bio = "Programmer, Full-stack Developer, Software Engineer";
 export const avatar = "https://avatars.githubusercontent.com/u/67628903";
