656b7a951935b71431aa00b7737341823e32a96b
Shihab Mahamud <shihab4t@gmail.com>
2025-07-30 03:31:29 +0600

    dark code

--- src/components/screens/skills/skills-items.tsx
+++ src/components/screens/skills/skills-items.tsx
@@ -5,12 +5,17 @@ export const SkillsItems = () => {
         <ul className="space-y-1">
             {skills.map((skill) => (
                 <li key={skill.title} className="flex flex-wrap space-x-2">
-                    <div className="font-semibold text-black">{skill.title}:</div>
+                    <div className="font-semibold text-black dark:text-white">{skill.title}:</div>
                     <div className="flex flex-wrap space-x-2">
                         {skill.items.map((item, index) => (
-                            <div key={index} className="text-black">
+                            <div key={index} className="">
                                 {item.name}
-                                {item.duration && <span className="font-normal text-gray-700"> ({item.duration})</span>}
+                                {item.duration && (
+                                    <span className="font-normal text-gray-700 dark:text-gray-400">
+                                        {" "}
+                                        ({item.duration})
+                                    </span>
+                                )}
                                 {index !== skill.items.length - 1 && ","}
                             </div>
                         ))}
