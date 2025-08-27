b3958438ab0979471c831ac6fe3dc86df78e82e1
Shihab Mahamud <shihab4t@gmail.com>
2025-08-27 16:53:36 +0600

    profile

--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -1,9 +1,11 @@
+import { email, name, role, slogan } from "../information";
+
 export const PROFILE_INFO = {
-    role: "Programmer | Full-stack Developer | Software Engineer",
-    logo: "Shihab Mahamud",
-    slogan: "Solving, Coding, Deploying...",
-    displayName: "Shihab Mahamud",
-    email: "boumedienelouai2@gmail.com",
+    role: role,
+    logo: name,
+    slogan: slogan,
+    displayName: name,
+    email: email,
     about: `
 I'm a **software engineer** passionate about crafting high-quality digital experiences. With expertise in **full-stack** development and a strong eye for **UI/UX**, I build **performant, pixel-perfect** applications that are both functional and visually appealing.  
 Always **learning**, always **iterating**, always pushing boundaries.  
---
+++ src/lib/information.ts
@@ -0,0 +1,7 @@
+export const name = "Shihab Mahamud";
+export const role = "Programmer | Full-stack Developer | Software Engineer";
+export const slogan = "Solving, Coding, Deploying...";
+export const email = "mt.shihab26@gmail.com";
+export const twitter = "https://x.com/mt_shihab26";
+export const linkedin = "https://linkedin.com/in/mt-shihab26";
+export const github = "https://github.com/mt-shihab26";
