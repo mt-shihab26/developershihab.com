6992d9c15e8481d20537a138d5e21f2334f2aa05
Shihab Mahamud <shihab4t@gmail.com>
2025-08-28 17:06:05 +0600

    skills

---
+++ src/lib/skills.ts
@@ -0,0 +1,111 @@
+import {
+    DigitalOcean,
+    Git,
+    Go,
+    JavaScript,
+    Laravel,
+    Linux,
+    MySQL,
+    NeoVim,
+    NodeJs,
+    PHP,
+    React,
+    Redis,
+    ShadcnUI,
+    TailwindCSS,
+    TypeScript,
+    WordPress,
+} from "developer-icons";
+
+export const skills = {
+    languages: [
+        {
+            title: "PHP",
+            href: "https://www.php.net",
+            icon: PHP,
+        },
+        {
+            title: "TypeScript",
+            href: "https://www.typescriptlang.org",
+            icon: TypeScript,
+        },
+        {
+            title: "JavaScript",
+            href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+            icon: JavaScript,
+        },
+        {
+            title: "Go",
+            href: "https://go.dev",
+            icon: Go,
+        },
+    ],
+    frameworks: [
+        {
+            title: "Laravel",
+            href: "https://laravel.com",
+            icon: Laravel,
+        },
+        {
+            title: "React",
+            href: "https://react.dev",
+            icon: React,
+        },
+        {
+            title: "Inertia.js",
+            href: "https://inertiajs.com/",
+        },
+        {
+            title: "Tailwind CSS",
+            href: "https://tailwindcss.com/",
+            icon: TailwindCSS,
+        },
+        {
+            title: "shadcn/ui",
+            href: "https://ui.shadcn.com/",
+            icon: ShadcnUI,
+        },
+        {
+            title: "WordPress",
+            href: "https://wordpress.org",
+            icon: WordPress,
+        },
+        {
+            title: "Node.js",
+            href: "https://nodejs.org/",
+            icon: NodeJs,
+        },
+    ],
+    others: [
+        {
+            title: "MySQL",
+            href: "https://www.mysql.com",
+            icon: MySQL,
+        },
+        {
+            title: "Redis",
+            href: "https://redis.io/",
+            icon: Redis,
+        },
+        {
+            title: "Git",
+            href: "https://git-scm.com/",
+            icon: Git,
+        },
+        {
+            title: "Linux",
+            href: "https://www.kernel.org",
+            icon: Linux,
+        },
+        {
+            title: "Neovim",
+            href: "https://neovim.io",
+            icon: NeoVim,
+        },
+        {
+            title: "DigitalOcean",
+            href: "https://digitalocean.com",
+            icon: DigitalOcean,
+        },
+    ],
+};
