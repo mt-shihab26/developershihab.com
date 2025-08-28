9cb70a50896ceb0586964c55de49b96cc13214b1
Shihab Mahamud <shihab4t@gmail.com>
2025-08-28 14:18:11 +0600

    resume

---
+++ src/lib/resume.tsx
@@ -0,0 +1,37 @@
+import { renderToString } from "react-dom/server";
+import { pdf } from "./pdf";
+
+import { Resume } from "@/components/screens/resume";
+
+const flags = () => {
+    return {
+        image: false,
+    };
+};
+
+const base = (resume: string) => {
+    return `
+        <!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>shihab-mahamud-resume.pdf</title>
+                <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+            </head>
+            <body>${resume}</body>
+        </html>
+    `;
+};
+
+const render = async () => {
+    const image = flags().image;
+
+    const resume = renderToString(<Resume image={image} />);
+    return base(resume);
+};
+
+const html = await render();
+const path = "public/shihab-mahamud-resume.pdf";
+
+await pdf(html, path);
