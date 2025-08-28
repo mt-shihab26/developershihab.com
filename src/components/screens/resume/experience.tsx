dbeed84c1e5cdfcf4579c4d3fc3a741ed7fbd3fb
Shihab Mahamud <shihab4t@gmail.com>
2025-08-28 17:38:32 +0600

    experience

--- src/components/screens/resume/experience.tsx
+++ src/components/screens/resume/experience.tsx
@@ -25,8 +25,8 @@ export const Experience = () => {
                     <div className="relative space-y-3 pl-1 before:absolute before:left-2.5 before:h-full before:w-px before:bg-gray-300 dark:before:bg-gray-600">
                         {e.positions.map((position, positionIndex) => (
                             <div key={positionIndex} className="space-y-2">
-                                <div className="relative z-10 flex items-center space-x-2">
-                                    <Code2 className="size-3" />
+                                <div className="relative z-10 flex items-center space-x-3">
+                                    <Code2 className="h-4 w-4 bg-white dark:bg-black" />
                                     <h3 className="text-sm font-semibold text-gray-900 dark:text-white">
                                         {position.title}
                                     </h3>
