965780cd2d90d286adf1a9388167e2b8f46263ad
Shihab Mahamud <shihab4t@gmail.com>
2025-09-01 16:13:54 +0600

    wip

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,27 +4,24 @@ import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 
 export async function GET(context) {
-    const blog = await getCollection("blog", entry => !entry.data.draft);
-    const projects = await getCollection("projects", entry => !entry.data.draft);
+    const blog = (await getCollection("blog", entry => !entry.data.draft)).map(post => ({
+        title: post.data.title,
+        pubDate: post.data.pubDatetime,
+        description: post.data.description,
+        link: `/articles/${post.id}/`,
+        categories: [post.data.category, ...post.data.tags],
+    }));
 
-    const items = [
-        ...blog.map(post => ({
-            title: post.data.title,
-            pubDate: post.data.pubDatetime,
-            description: post.data.description,
-            link: `/articles/${post.id}/`,
-            categories: [post.data.category, ...post.data.tags],
-        })),
-        ...projects.map(project => ({
-            title: project.data.name,
-            pubDate: new Date(),
-            description: project.data.description,
-            link: `/projects/${project.id}/`,
-            categories: ["project", ...project.data.technologies],
-        })),
-    ];
+    // const projects = (await getCollection("projects", entry => !entry.data.draft)).map(project => ({
+    //     title: project.data.name,
+    //     pubDate: new Date(),
+    //     description: project.data.description,
+    //     link: `/projects/${project.id}/`,
+    //     categories: ["project", ...project.data.technologies],
+    // }));
 
-    // Sort by publication date (newest first)
+    const items = [...blog /*...projects */];
+
     items.sort((a, b) => b.pubDate - a.pubDate);
 
     return rss({
