d0a5897d00c041cc9570b3cd0e39c0338ee2e95c
Shihab Mahamud <shihab4t@gmail.com>
2025-09-04 17:39:39 +0600

    resume

--- src/components/screens/resume/projects.tsx
+++ src/components/screens/resume/projects.tsx
@@ -17,25 +17,22 @@ export const Projects = () => {
                         <div className="border-b border-gray-200 pb-3 last:border-b-0 dark:border-gray-700">
                             <div className="flex items-center space-x-3">
                                 <span className="h-2 w-2 rounded-full bg-gray-600" />
-                                {project.preview ? (
-                                    <a
-                                        href={project.preview || project.sourceCode}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="text-sm font-bold text-gray-900 hover:underline dark:text-white"
-                                    >
-                                        {project.name}
-                                    </a>
-                                ) : (
-                                    <h3 className="text-sm font-bold text-gray-900 dark:text-white">
-                                        {project.name}
-                                    </h3>
-                                )}
+                                <h3 className="text-sm font-bold text-gray-900 dark:text-white">{project.name}</h3>
                             </div>
                             <div className="relative space-y-1 pl-1 before:absolute before:left-3 before:h-full before:w-px before:bg-gray-300 dark:before:bg-gray-600">
                                 <div className="relative z-10 flex items-center justify-between">
                                     <div className="flex items-center space-x-3">
                                         <Code2 className="h-4 w-4 bg-white dark:bg-black" />
+                                        {project.preview && (
+                                            <a
+                                                href={project.preview}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="text-xs text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+                                            >
+                                                {project.preview?.split("//")[1]}
+                                            </a>
+                                        )}
                                         {project.sourceCode && (
                                             <a
                                                 href={project.sourceCode}
