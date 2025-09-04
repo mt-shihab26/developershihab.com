8052fdae9584c79700d55ef524cb22fffd89bcbf
Shihab Mahamud <shihab4t@gmail.com>
2025-09-04 17:10:09 +0600

    page break

--- src/components/screens/resume/projects.tsx
+++ src/components/screens/resume/projects.tsx
@@ -1,5 +1,6 @@
 import { getProjectsRow } from "@/lib/projects";
 
+import { Fragment } from "react";
 import { Heading } from "./heading";
 
 export const Projects = () => {
@@ -10,41 +11,46 @@ export const Projects = () => {
             <Heading>Projects</Heading>
             <div className="space-y-3">
                 {projects.map((project, index) => (
-                    <div key={index} className="border-b border-gray-200 pb-3 last:border-b-0 dark:border-gray-700">
-                        <div className="mb-2 flex items-center justify-between">
-                            <div className="flex items-center space-x-3">
-                                <span className="h-2 w-2 rounded-full bg-gray-600" />
-                                {project.preview ? (
+                    <Fragment key={index}>
+                        {index === 2 && <div className="print:[page-break-before:always]"></div>}
+                        <div className="border-b border-gray-200 pb-3 last:border-b-0 dark:border-gray-700">
+                            <div className="mb-2 flex items-center justify-between">
+                                <div className="flex items-center space-x-3">
+                                    <span className="h-2 w-2 rounded-full bg-gray-600" />
+                                    {project.preview ? (
+                                        <a
+                                            href={project.preview || project.sourceCode}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="text-sm font-bold text-gray-900 hover:underline dark:text-white"
+                                        >
+                                            {project.name}
+                                        </a>
+                                    ) : (
+                                        <h3 className="text-sm font-bold text-gray-900 dark:text-white">
+                                            {project.name}
+                                        </h3>
+                                    )}
+                                </div>
+                                {project.sourceCode && (
                                     <a
-                                        href={project.preview || project.sourceCode}
+                                        href={project.sourceCode}
                                         target="_blank"
                                         rel="noopener noreferrer"
-                                        className="text-sm font-bold text-gray-900 hover:underline dark:text-white"
+                                        className="text-xs text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
                                     >
-                                        {project.name}
+                                        {project.sourceCode?.split("//")[1]}
                                     </a>
-                                ) : (
-                                    <h3 className="text-sm font-bold text-gray-900 dark:text-white">{project.name}</h3>
                                 )}
                             </div>
-                            {project.sourceCode && (
-                                <a
-                                    href={project.sourceCode}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="text-xs text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
-                                >
-                                    {project.sourceCode?.split("//")[1]}
-                                </a>
+                            {project.technologies.length > 0 && (
+                                <div className="mb-1 pl-6 text-xs text-gray-600 dark:text-gray-400">
+                                    ({project.technologies.join(", ")})
+                                </div>
                             )}
+                            <p className="pl-6 text-xs text-gray-800 dark:text-white">{project.description}</p>
                         </div>
-                        {project.technologies.length > 0 && (
-                            <div className="mb-1 pl-6 text-xs text-gray-600 dark:text-gray-400">
-                                ({project.technologies.join(", ")})
-                            </div>
-                        )}
-                        <p className="pl-6 text-xs text-gray-800 dark:text-white">{project.description}</p>
-                    </div>
+                    </Fragment>
                 ))}
             </div>
         </section>
