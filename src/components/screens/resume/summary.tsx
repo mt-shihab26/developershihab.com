7d9b95661b851d8393ce8369a790450359a545fd
Shihab Mahamud <shihab4t@gmail.com>
2026-04-26 16:42:22 +0600

    summary

--- src/components/screens/resume/summary.tsx
+++ src/components/screens/resume/summary.tsx
@@ -6,24 +6,24 @@ export const Summary = () => {
             <Heading>Summary</Heading>
             <div className="space-y-1.5">
                 <div>
-                    Results-driven Software Engineer with <span className="font-medium">3+ years of experience</span> in{" "}
-                    <span className="font-medium">full-stack development</span> and building{" "}
-                    <span className="font-medium">scalable software solutions</span>. Skilled in architecting,
+                    Results-driven Software Engineer with <span className="font-semibold">3+ years of experience</span>{" "}
+                    in <span className="font-semibold">full-stack development</span> and building{" "}
+                    <span className="font-semibold">scalable software solutions</span>. Skilled in architecting,
                     developing, and maintaining high-performance applications that align with both technical and
                     business objectives.
                 </div>
                 <div>
                     Proficient in modern web technologies, including{" "}
-                    <span className="font-medium">
+                    <span className="font-semibold">
                         PHP, Laravel, JavaScript/TypeScript, React, TailwindCSS, and WordPress
                     </span>
-                    . Experienced in delivering <span className="font-medium">end-to-end solutions</span>, from
-                    conception to <span className="font-medium">cloud deployment</span>, with a strong focus on rapid
-                    development and <span className="font-medium">clean, maintainable code</span>.
+                    . Experienced in delivering <span className="font-semibold">end-to-end solutions</span>, from
+                    conception to <span className="font-semibold">cloud deployment</span>, with a strong focus on rapid
+                    development and <span className="font-semibold">clean, maintainable code</span>.
                 </div>
                 <div>
                     Strong background in problem-solving and analytical thinking, with over{" "}
-                    <span className="font-medium">1,000 problems solved</span> on online coding platforms. Experienced
+                    <span className="font-semibold">1,000 problems solved</span> on online coding platforms. Experienced
                     in collaborating with senior developers, applying industry best practices, and staying up to date
                     with emerging technologies to deliver innovative and efficient solutions.
                 </div>
